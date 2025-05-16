fe23e77cecf4b0949b650a917ef11fe4edeb697f
2205103070002 <2205103070002@paruluniversity.ac.in>
2025-05-16 11:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
 
-}
+    function getFormvalue(event) {
+      
+      event.preventDefault();
+
+      
+      const firstName = document.forms["nameForm"]["fname"].value.trim();
+      const lastName = document.forms["nameForm"]["lname"].value.trim();
+
+    
+      alert(`${firstName} ${lastName}`);
+    }
+
+    document.getElementById("nameForm").addEventListener("submit", getFormvalue);
